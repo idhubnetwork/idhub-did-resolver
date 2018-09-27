9fabf4d90ff9d1c57151567f9b445d6e0d547439
piouslove <305053045@qq.com>
2018-09-27 13:53:17 +0800

    did view function

---
+++ src/views.go
@@ -0,0 +1,38 @@
+package resolver
+
+import "github.com/ethereum/go-ethereum/common"
+
+func (r *resolver) IdentityOwner(address string) string {
+	instance := r.contract
+	identity := common.HexToAddress(address)
+	owner, err := instance.IdentityOwner(nil, identity)
+	if err != nil {
+	}
+	return owner.Hex()
+}
+
+func (r *resolver) ValidPublicKey(address, keyType, key string) bool {
+	instance := r.contract
+	identity := common.HexToAddress(address)
+	publickKeyType := [32]byte{}
+	copy(publickKeyType[:], keyType)
+	publickKey := [32]byte{}
+	copy(publickKey[:], key)
+	ok, err := instance.ValidPublicKey(nil, identity, publickKeyType, publickKey)
+	if err != nil {
+	}
+	return ok
+}
+
+func (r *resolver) ValidAuthentication(address, keyType, key string) bool {
+	instance := r.contract
+	identity := common.HexToAddress(address)
+	authenticationType := [32]byte{}
+	copy(authenticationType[:], keyType)
+	authentication := [32]byte{}
+	copy(authentication[:], key)
+	ok, err := instance.ValidPublicKey(nil, identity, authenticationType, authentication)
+	if err != nil {
+	}
+	return ok
+}
